8c339ace3cf2c870ac6017d8cb866b4b3b31de10
chris <chrischen2018@163.com>
2019-06-04 16:34:01 +0800

    add admin

---
+++ vue-cli-demo-oldversion/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <ChrisComp/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import ChrisComp from './components/ChrisComponent'
+
+export default {
+  name: 'App',
+  components: {
+    ChrisComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-demo-oldversion/src/components/ChrisComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'ChrisComp',
+  data () {
+    return {
+      msg: '我的第一个 Vue-Cli'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  color: blue;
+  font-weight: normal;
+}
+
+</style>
